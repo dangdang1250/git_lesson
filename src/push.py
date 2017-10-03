1ac82385099bd662527b3db34ce7ac3ffe511af1
dangdang1250 <chengjixuan@hotmail.com>
2017-10-03 04:42:33 -0400

    learning how to do logging in python from Corey Schafer

--- src/push.py
+++ src/push.py
@@ -4,10 +4,15 @@
 import os
 import csv
 import time
+import logging
+
+#logging config
+logging.basicConfig(filename='test.log', level=logging.DEBUG,
+   format='%(asctime)s:%(levename)s:%(message)s')
 
 SRC_PATH = os.getcwd()
 os.chdir(r'/home/jicheng/MY20')
-print(os.getcwd())
+logging.debug(os.getcwd())
 time.sleep(1)
 PUSH_CMD = []
 
@@ -17,9 +22,10 @@ with open(FILEPATH,'r') as data_file:
    for line in CSV_DATA:
       if(line[0] != "#"):
          PUSH_CMD.append('adb push {} {}'.format(line[0].strip(), line[1].strip()))
+         logging.debug('adb push {} {}'.format(line[0].strip(),line[1].strip()))
 
 for cmd in PUSH_CMDS:
-   print(cmd)
+   logging.debug(cmd)
    os.system(cmd)
    time.sleep(2)
 
